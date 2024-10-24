57b250af979ac4b0d810b92b72a9d065ca9f3900
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2024-10-24 17:16:10 -0300

    finishing instagram profile view

--- hackatruck/aula01/aula01/instagramProfile.swift
+++ hackatruck/aula01/aula01/instagramProfile.swift
@@ -1,29 +1,25 @@
-//
-//  instagramProfile.swift
-//  aula01
-//
-//  Created by Turma01-9 on 23/10/24.
-//
-
 import SwiftUI
 
 struct instagramProfile: View {
     var body: some View {
-        VStack{
+        VStack(spacing: 1){
             HStack{
                 Text("username")
-                    .padding(.leading, 30)
-                    .padding(.bottom, 30)
-                
-                
+                    .padding(.leading, 20)
+                    .bold()
                 Spacer()
-            }
+                Image(systemName: "paperplane").padding(.trailing,10)
+                Image(systemName: "ellipsis").padding(.trailing,20)
+                
+                
+            }.padding(.bottom, 30)
             HStack{
                 Image(.girassol)
                     .resizable()
                     .clipShape(Circle())
                 
                     .frame(width: 130, height: 130)
+                    .overlay(Image(systemName: "plus.circle.fill").resizable().frame(width: 30, height: 30).offset(x:50,y:50))
                 VStack{
                     HStack{
                         VStack{
@@ -42,19 +38,49 @@ struct instagramProfile: View {
                     Text("Follow").frame(width:220, height: 30).background(.gray).cornerRadius(5)
                 }
             }
-            HStack{
-                VStack{
-                    
-                    Text("Nome Sobrenome")
-                    
+            VStack{
+                HStack{
+                    Text("Nome Sobrenome").padding(.leading,20).padding(.top,15)
+                    Spacer()
                 }
+                HStack{
+                    Text("Biografia Biografia Biografia").padding(.leading,20)
+                    Spacer()
+                }
+            }
+            HStack{
+                Spacer()
+                Image(systemName: "square.grid.3x3.fill").padding(.top, 80)
+                Spacer()
+                Spacer()
+                Image(systemName: "person.crop.rectangle").padding(.top,80)
+                Spacer()
+            }.padding(.bottom, 10)
+            HStack(spacing:1){
                 
+                Text("").frame(width: 140, height: 140).background(.gray)
+                    
+                Text("").frame(width: 140, height: 140).background(.gray)
+                    
+                Text("").frame(width: 140, height: 140).background(.gray)
+                
+            }
+            HStack(spacing:1){
+                
+                Text("").frame(width: 140, height: 140).background(.gray)
+                    
+                Text("").frame(width: 140, height: 140).background(.gray)
+                    
+                Text("").frame(width: 140, height: 140).background(.gray)
+                
+            }
+            
+            
             }
             Spacer()
             
         }
     }
-}
 
 #Preview {
     instagramProfile()
